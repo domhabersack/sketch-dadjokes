8c73f1e055b90137fa07fef9fdc2efcf059b8607
Dom Habersack <dominik.habersack@netlight.com>
2019-11-02 17:05:50 +0100

    Initial commit

---
+++ src/dad-jokes.js
@@ -0,0 +1,29 @@
+const sketch = require('sketch')
+const { DataSupplier } = sketch
+const util = require('util')
+
+export function onStartup () {
+  DataSupplier.registerDataSupplier('public.text', 'Dad Jokes', 'SupplyData')
+}
+
+export function onShutdown () {
+  DataSupplier.deregisterDataSuppliers()
+}
+
+export function onSupplyData (context) {
+  let dataKey = context.data.key
+  const items = util.toArray(context.data.items).map(sketch.fromNative)
+
+  items.forEach((item, index) => {
+    fetch('https://icanhazdadjoke.com', {
+      headers: {
+        'Accept': 'application/json',
+        'User-Agent': 'Sketch Data Dad Jokes (https://islovely.co/projects/sketch-data-dad-jokes)'
+      }
+    }).then(response => response.json()).then(({ joke }) => {
+      DataSupplier.supplyDataAtIndex(dataKey, joke, index)
+    })
+  }).catch(error => {
+    log(error)
+  })
+}
